4cc98d0d017966c7af2e732c50e620077a6cd1e9
신지영 <sinjiyoung@sinjiyeong-ui-MacBook-Pro.local>
2019-11-23 04:14:56 +0900

    data sample

--- Home.js
+++ Home.js
@@ -17,7 +17,20 @@ export default class Home extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      items: {}
+      items: {
+        "2017-06-14": [
+          {
+            "height": 50,
+            "name": "0614",
+          }
+        ],
+        "2017-06-17": [
+          {
+            "height": 50,
+            "name": "0617",
+          }
+        ]
+      }
     };
   }
 
@@ -25,42 +38,42 @@ export default class Home extends Component {
     return (
       <Agenda
         items={this.state.items}
-        loadItemsForMonth={this.loadItems.bind(this)}
+        // loadItemsForMonth={this.loadItems.bind(this)}
         selected={'2017-05-16'}
         renderItem={this.renderItem.bind(this)}
-        renderEmptyDate={this.renderEmptyDate.bind(this)}
+        renderEmptyData={this.renderEmptyData.bind(this)}
         rowHasChanged={this.rowHasChanged.bind(this)}
 
       />
     );
   }
 
-  loadItems(day) {
+  // loadItems(day) {
 
-    setTimeout(() => {
-      for (let i = -15; i < 85; i++) {
-        const time = day.timestamp + i * 24 * 60 * 60 * 1000;
-        const strTime = this.timeToString(time);
-        if (!this.state.items[strTime]) {
-          this.state.items[strTime] = [];
-          const numItems = Math.floor(Math.random() * 5);
-          for (let j = 0; j < numItems; j++) {
-            this.state.items[strTime].push({
-              name: 'Item for ' + strTime,
-              height: Math.max(50, Math.floor(Math.random() * 150))
-            });
-          }
-        }
-      }
-      console.log(this.state.items);
-      const newItems = {};
-      Object.keys(this.state.items).forEach(key => {newItems[key] = this.state.items[key];});
-      this.setState({
-        items: newItems
-      });
-    }, 1000);
-    console.log(`Load Items for ${day.year}-${day.month}`);
-  }
+  //   setTimeout(() => {
+  //     for (let i = -15; i < 85; i++) {
+  //       const time = day.timestamp + i * 24 * 60 * 60 * 1000;
+  //       const strTime = this.timeToString(time);
+  //       if (!this.state.items[strTime]) {
+  //         this.state.items[strTime] = [];
+  //         const numItems = Math.floor(Math.random() * 5);
+  //         for (let j = 0; j < numItems; j++) {
+  //           this.state.items[strTime].push({
+  //             name: 'Item for ' + strTime,
+  //             height: Math.max(50, Math.floor(Math.random() * 150))
+  //           });
+  //         }
+  //       }
+  //     }
+  //     console.log(this.state.items);
+  //     const newItems = {};
+  //     Object.keys(this.state.items).forEach(key => {newItems[key] = this.state.items[key];});
+  //     this.setState({
+  //       items: newItems
+  //     });
+  //   }, 1000);
+  //   console.log(`Load Items for ${day.year}-${day.month}`);
+  // }
 
   renderItem(item) {
     return (
@@ -74,7 +87,7 @@ export default class Home extends Component {
     );
   }
 
-  renderEmptyDate() {
+  renderEmptyData() {
     return (
       <View style={styles.emptyDate}>
         <Button
@@ -89,10 +102,10 @@ export default class Home extends Component {
     return r1.name !== r2.name;
   }
 
-  timeToString(time) {
-    const date = new Date(time);
-    return date.toISOString().split('T')[0];
-  }
+  // timeToString(time) {
+  //   const date = new Date(time);
+  //   return date.toISOString().split('T')[0];
+  // }
 }
 
 const styles = StyleSheet.create({
